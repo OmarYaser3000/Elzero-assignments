fc20a19c8840f9543082eaca1f295e5760cb55be
OmarYaser <expertomar3000@gmail.com>
2023-07-30 14:49:34 +0200

    edited one solution and finished till RE

--- JS_pt01/17.js
+++ JS_pt01/17.js
@@ -1,9 +1,3 @@
 let maxSafeInteger = Number.MAX_SAFE_INTEGER;
-console.log(
-  Math.round(
-    Math.pow(
-      Number.MAX_SAFE_INTEGER * Number.EPSILON,
-      true + true + true + true
-    )
-  )
-);
+
+console.log(maxSafeInteger.toString().split("").length);
