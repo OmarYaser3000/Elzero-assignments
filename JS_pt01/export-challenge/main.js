dc8f32af6aa1eca82f8bbf00529745422b9c0760
OmarYaser <expertomar3000@gmail.com>
2023-08-03 15:44:04 +0200

    finished js assignments

---
+++ JS_pt01/export-challenge/main.js
@@ -0,0 +1,4 @@
+import calc from "./mod-one.js";
+import { modOne } from "./mod-one.js";
+
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
---
+++ JS_pt01/export-challenge/mod-one.js
@@ -0,0 +1,11 @@
+import { numOne, numTwo, numThree } from "./mod-two.js";
+
+export default function (firstNum, secNum, thirdNum) {
+  return firstNum + secNum + thirdNum;
+}
+
+export const modOne = {
+  numOne: numOne,
+  numTwo: numTwo,
+  numThree: numThree,
+};
